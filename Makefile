7a52ecc47c481aca2d1da4617210e1741031d442
Brandon Wie <brandonwie.cs@gmail.com>
2023-11-03 16:43:51 +0900

    Remove unused files

--- Makefile
+++
@@ -1,5 +0,0 @@
-dev:
-	@echo "Starting Flask dev server..."
-	@FLASK_APP=server.py FLASK_ENV=developement flask run
-
-.PHONY: dev
--- server.py
+++ server.py
@@ -27,4 +27,4 @@ def get_weather():
 )
 
 if __name__ == '__main__':
-    serve(app, host="0.0.0.0",port=3000,debug=True)
+    serve(app, host="0.0.0.0",port=3000,debug=False)
